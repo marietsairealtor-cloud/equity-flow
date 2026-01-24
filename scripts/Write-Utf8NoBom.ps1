d2bb5ad1dd28c1e28966735b21f15fc736cf0410
Marie <marie.systems@gmail.com>
2026-01-24 18:58:21 -0500

    Dev tooling: add UTF8-no-BOM writer for migrations

---
+++ scripts/Write-Utf8NoBom.ps1
@@ -0,0 +1,7 @@
+﻿param(
+  [Parameter(Mandatory=$true)][string]$Path,
+  [Parameter(Mandatory=$true)][string]$Content
+)
+$dir = Split-Path -Parent $Path
+if ($dir -and !(Test-Path $dir)) { New-Item -ItemType Directory -Force -Path $dir | Out-Null }
+[System.IO.File]::WriteAllText($Path, $Content, (New-Object System.Text.UTF8Encoding($false)))
